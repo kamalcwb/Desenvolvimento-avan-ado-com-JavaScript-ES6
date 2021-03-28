1181116b7d0e483ff3ce6897c8e669316fefef2f
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-28 15:53:27 -0300

    update

---
+++ Conheça Rest e Spread Operator.js
@@ -0,0 +1,42 @@
+function sum(a, b) {
+    var value = 0
+
+    for (var i = 0; i < arguments.length; i++) {
+        value += arguments[i]
+    }
+    return value
+}
+console.log(sum(5, 5, 6, 3, 7))
+//Modo ECMA6 REST OPERATOR
+function sum(...args) {
+    return args.reduce((acc, value) => acc + value, 0)
+}
+console.log(sum(5, 5, 6, 3, 7))
+
+const sum = (a, b, ...rest) => {
+
+}
+
+const mut = (...args) => args.reduce((acc, value) => acc * value, 1)
+//SPREAD OPERATOR
+//STRINGS, ARRAYS, LITERAL OBJECTS E OBEJETOS ITERAVEIS
+const str = 'Digital Inovation One'
+
+function logArgs(...args) {
+    console.log(args)
+}
+logArgs(...str)
+
+const arr = [1, 2, 3, 4, 5]
+logArgs(...arr)
+
+const arr2 = [...arr, 6, 7, 8, 9] // concatena o arr com o arr2
+
+const obj = {
+    teste: 123
+}
+const obj2 = {
+    ...obj,
+    teste2: 'Ola'
+}
+console.log(obj2)
--- Enhanced Object Literals.js
+++ Enhanced Object Literals.js
@@ -47,3 +47,44 @@ var obj = {
     [propName + 'concat']: 'prop value'
 }
 console.log(obj)
+
+/*
+Podemos referenciar outro argumento como valor padrão para um argumento?
+R: Sim, mas apenas se o argumento vier anteriormente ao que está sendo atribuído.
+
+Qual a forma de criar uma função construtora com arrow functions?
+R: Não podemos criar funções construtoras usando arrow functions.
+
+Podemos realizar expressões para definir nomes de atributos?
+R: Sim, utilizando colchetes.
+
+Quando podemos omitir os parênteses de uma arrow function?
+R: Quando existir apenas um argumento e nada mais.
+
+Qual a forma tradicional de escrever funções, antes de existirem arrow functions?
+R: A palavra function, o seu nome de maneira opcional, os parênteses e as chaves para o corpo.
+
+Qual a forma de atribuir um valor padrão ao argumento de uma função que surgiu com o ES6:
+R: Usando o caractere '=' seguido do valor que queremos atribuir ao argumento.
+
+O que é 'lazy evaluation'?
+R: A característica que permite podermos utilizar funções para definir valores de um argumento e a mesma só será invocada quando o
+argumento for indefinido.
+
+Quando podemos omitir o valor de uma propriedade ou método ao definir um objeto literal?
+R: Quando o valor vier de uma variável com o mesmo nome da propriedade ou método.
+
+As arrow functions são sempre:
+R: Funções anônimas.
+
+var obj = {
+  sleep: function() {
+    setTimeout(() => {
+      console.log(this);
+    }, 1000);
+  }
+}
+
+obj.sleep();
+R: O objeto 'obj'.
+*/
