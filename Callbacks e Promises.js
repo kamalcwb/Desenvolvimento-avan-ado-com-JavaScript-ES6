8ea55f24c9fab18ae6f9b13c9a1b7a167e42ea74
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-28 17:17:03 -0300

    Create Callbacks e Promises.js

---
+++ Callbacks e Promises.js
@@ -0,0 +1,56 @@
+//callbacks
+function doSomething(callback) {
+    setTimeout(function () {
+        //did something
+        callback('First data')
+    }, 1000)
+}
+
+function doOtherthing(callback) {
+    setTimeout(function () {
+        //did something
+        callback('Second data')
+    }, 1000)
+}
+
+function doALL() {
+    doSomething(function (data) {
+        var processedData = data.split('')
+
+        doOtherthing(function (data2) {
+            var processedData2 = data2.split('')
+
+            setTimeout(function () {
+                console.log(processedData, processedData2)
+            }, 1000)
+        })
+    })
+}
+
+doALL()
+
+//promisses
+const doSomeThingPromise = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        //did something
+        resolve('Fist data')
+    }, 1000)
+})
+
+const doOtherThingPromise = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        //did something
+        resolve('Second data')
+    }, 1000)
+})
+
+console.log(doSomeThingPromise)
+doSomeThingPromise.then(data => console.log(data))
+doSomeThingPromise
+    .then(data => { console.log(data); return doOtherThingPromise })
+    .then(data2 => console.log(data2))
+    .catch(error => console.log('ops, error'))
+
+Promise.all([doOtherThingPromise(), doOtherThingPromise()]).then((data => {
+    console.log(data)
+}))
